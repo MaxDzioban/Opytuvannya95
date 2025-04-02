1d855eef7b57869a3c34b1a7c0409d5749c4b0ea
shshrg <sasha.shergina@gmail.com>
2025-04-02 11:27:41 +0300

    added prikol

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ document.addEventListener('keydown', (event) => {
 });
 
 
+const bannerWindow = document.querySelector('.banner-window');
+
+if (bannerWindow) {
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    bannerWindow.addEventListener('mousedown', (event) => {
+        isDragging = true;
+        offsetX = event.clientX - bannerWindow.offsetLeft;
+        offsetY = event.clientY - bannerWindow.offsetTop;
+        bannerWindow.style.cursor = 'grabbing';
+    });
+
+    document.addEventListener('mousemove', (event) => {
+        if (isDragging) {
+            bannerWindow.style.left = `${event.clientX - offsetX}px`;
+            bannerWindow.style.top = `${event.clientY - offsetY}px`;
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+        bannerWindow.style.cursor = 'grab';
+    });
+}
