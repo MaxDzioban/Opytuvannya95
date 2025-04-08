19481a03a3cba7df07b64202f384c499d7196afe
shshrg <sasha.shergina@gmail.com>
2025-04-08 20:33:13 +0300

    changed some style

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
         event.preventDefault(); // Prevent the page from scrolling
@@ -14,30 +17,3 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
-
-const bannerWindow = document.querySelector('.banner-window');
-
-if (bannerWindow) {
-    let isDragging = false;
-    let offsetX = 0;
-    let offsetY = 0;
-
-    bannerWindow.addEventListener('mousedown', (event) => {
-        isDragging = true;
-        offsetX = event.clientX - bannerWindow.offsetLeft;
-        offsetY = event.clientY - bannerWindow.offsetTop;
-        bannerWindow.style.cursor = 'grabbing';
-    });
-
-    document.addEventListener('mousemove', (event) => {
-        if (isDragging) {
-            bannerWindow.style.left = `${event.clientX - offsetX}px`;
-            bannerWindow.style.top = `${event.clientY - offsetY}px`;
-        }
-    });
-
-    document.addEventListener('mouseup', () => {
-        isDragging = false;
-        bannerWindow.style.cursor = 'grab';
-    });
-}
