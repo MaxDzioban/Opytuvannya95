3aa0249cacd287da0401763635a0771380101715
MaxDzioban <149308245+MaxDzioban@users.noreply.github.com>
2025-05-18 20:08:47 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: 'client',
   plugins: [react()],
   build: {
-    outDir: "../server/client-build", // ✅ Збірка йде в папку серверу
+    outDir: "../server/client-build", // залишаємо як є
     emptyOutDir: true
   }
 });
-
