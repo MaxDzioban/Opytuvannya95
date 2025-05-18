3c24ada8c6daf5e3468593e8ce407b380f757570
MaxDzioban <149308245+MaxDzioban@users.noreply.github.com>
2025-05-19 00:27:21 +0300

    Update UserStuff.jsx

--- client/src/UserStuff.jsx
+++ client/src/UserStuff.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const UserIcon = ({ username }) => {
-    return (
-        <div className="user-label">
-            <img src="/User_with_computer.ico" alt="User Icon" id="user-icon" />
-            <p id="user-name">{username}</p>
-        </div>
-    )
-}
+  return (
+    <div className="user-label">
+      <img src="/User_with_computer.ico" alt="User Icon" id="user-icon" />
+      <p id="user-name">{username}</p>
+    </div>
+  );
+};
 
-export const Statistics = ({ username }) => {
-
-}
+export const Statistics = ({ username }) => {};
